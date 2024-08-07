dd015d0a4a6c8623f7bb1f43b2870fccb2d66e78
Avi Vahl <avi@vahl.co.il>
2024-08-07 20:45:28 +0300

    infra: reactivate lint for js/mjs/cjs files

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,33 @@
-import { fixupPluginRules } from "@eslint/compat";
-import pluginJs from "@eslint/js";
-import configPrettier from "eslint-config-prettier";
-import pluginNoOnlyTests from "eslint-plugin-no-only-tests";
-import pluginTypescript from "typescript-eslint";
+import { fixupPluginRules } from '@eslint/compat';
+import pluginJs from '@eslint/js';
+import configPrettier from 'eslint-config-prettier';
+import pluginNoOnlyTests from 'eslint-plugin-no-only-tests';
+import pluginTypescript from 'typescript-eslint';
+
+for (const config of pluginTypescript.configs.recommendedTypeChecked) {
+  config.files = ['**/*.{ts,tsx,mts,cts}']; // ensure config only targets TypeScript files
+}
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { ignores: ["dist/", "**/*.{js,mjs,cjs}"] },
+  { ignores: ['dist/'] },
   pluginJs.configs.recommended,
-
-  ...pluginTypescript.configs.recommendedTypeChecked,
-  { languageOptions: { parserOptions: { projectService: true } } },
-
-  { plugins: { "no-only-tests": fixupPluginRules(pluginNoOnlyTests) } },
-  configPrettier,
-
+  { plugins: { 'no-only-tests': fixupPluginRules(pluginNoOnlyTests) } },
   {
     rules: {
-      "no-only-tests/no-only-tests": "error",
+      // "no-console": "error",
+      'no-only-tests/no-only-tests': 'error',
+      'no-undef': 'off',
+      'no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
     },
   },
+  ...pluginTypescript.configs.recommendedTypeChecked,
+  { languageOptions: { parserOptions: { projectService: true } } },
+  {
+    files: ['**/*.{ts,tsx,mts,cts}'],
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+    },
+  },
+  configPrettier,
 ];
